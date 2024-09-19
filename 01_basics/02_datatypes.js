5fe2d8b543f94d46f69850faab6f42defc7256e4
Anurag Jaiswal <anuragjaiswal7355@gmail.com>
2024-09-19 15:12:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+// Best documentation for java script website is "TC39.ES"
+
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node js not browser because when we write a code on browser console its
+// show popup of 6 but in node js its show error 
+
+// NUMBER = 2 to power 53 it is range
+//string = ""
+//boolean = true/false
+//null it a standalone value null is a empty value
+//undefined => when we no assinged a value in a variable 
+//symbol => 
+// object
+
+console.log(typeof "Anurag jaiswal")
+console.log(typeof null) // its output is object
+console.log(typeof undefined) //its output is undefined 

1c19ee4902c7e2b7d745850ba8ce1f2da17cbaf9
Anurag Jaiswal <anuragjaiswal7355@gmail.com>
2024-09-19 15:40:26 +0000

    Operation

---
+++ 01_basics/04_Operation.js
@@ -0,0 +1,9 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "How are u"
+let str2 = " Anurag"
+console.log(str1+str2) // concatenation of two string are allow in javascript
+console.log("1" + 2) // in this line javascript automatically take 2 as a string and concate 12
+console.log(1 + "2") // same here also
